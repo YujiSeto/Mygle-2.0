fea9deb01f3f1d9b65bdcb5f79d4bd77f50a850d
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-03-22 21:13:39 +0900

    fix: comprehensive SSL attributes for secure transport

--- core/controller.php
+++ core/controller.php
@@ -13,10 +13,13 @@ class controller {
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 		);
 
-		// Se o host for TiDB Cloud, exigimos SSL
+		// Blunderbuss SSL for TiDB Cloud (Forces SSL handshake)
 		if (strpos($config['host'], 'tidbcloud.com') !== false) {
-			$options[1007] = file_exists('/etc/ssl/certs/ca-certificates.crt') ? '/etc/ssl/certs/ca-certificates.crt' : '';
-			$options[1014] = false;
+			$options[1007] = ''; // PDO::MYSQL_ATTR_SSL_CA
+			$options[1008] = ''; // PDO::MYSQL_ATTR_SSL_CAPATH
+			$options[1009] = ''; // PDO::MYSQL_ATTR_SSL_KEY
+			$options[1010] = ''; // PDO::MYSQL_ATTR_SSL_CERT
+			$options[1014] = false; // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT
 		}
 
 		$this->db = new PDO($dsn, $config['dbuser'], $config['dbpass'], $options);
